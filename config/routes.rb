459c1a4526ce99877d3cf5658f835cdac75681a1
Vallari Agrawal <val.agl002@gmail.com>
2022-09-07 00:26:07 +0530

    Create 'about' page route, view, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "products#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get '/about', to: "about#index"
+
   get "/products", to: "products#index"
   get "/:product_id/:item_id", to: "item#index"
   get "/:product_id", to: "products#show"
