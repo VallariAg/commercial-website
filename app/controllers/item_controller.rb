294ab4ecc663215ad28ac9131a945cf059e44351
Vallari Agrawal <val.agl002@gmail.com>
2022-09-06 23:58:01 +0530

    Design item index page with data imports

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,6 +2,8 @@ class ItemController < ApplicationController
   def index
     item_id = params[:item_id]
     product = YAML::load(File.open("data/products/" + params[:product_id] + ".yaml"))
+    @product_name = product[:title]
+    @product_route = "/" + product[:id]
     @item = product[:items][item_id]
   end
 end
